a50f736be4f086c6af39d51ebe603f2031ce1ee2
Osuka-Andala <teresa.andala@gmail.com>
2015-08-31 12:59:13 +0300

    Ran rake db:migrate and modified schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150831093650) do
+ActiveRecord::Schema.define(version: 20150831095543) do
 
   create_table "zombies", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20150831093650) do
     t.integer  "age"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email"
+    t.boolean  "rotting",    default: false
   end
 
 end
